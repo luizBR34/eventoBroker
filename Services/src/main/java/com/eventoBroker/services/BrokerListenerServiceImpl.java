cccd6ea251e3200edff3fe0393d64506f3fcca6b
Luiz Fernando Povoa <luizfer1@yahoo.com.br>
2021-06-30 19:29:47 -0300

    Adding filter to get know which message is ongoing.

--- Services/src/main/java/com/eventoBroker/services/BrokerListenerServiceImpl.java
+++ Services/src/main/java/com/eventoBroker/services/BrokerListenerServiceImpl.java
@@ -1,6 +1,9 @@
 package com.eventoBroker.services;
 
 import java.io.IOException;
+import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,6 +17,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import com.eventoApp.models.Event;
 import com.eventoApp.models.Guest;
@@ -29,18 +33,37 @@ public class BrokerListenerServiceImpl implements BrokerListenerService {
 	
 	ObjectMapper mapper = new ObjectMapper();
 	RestTemplate restTemplate = new RestTemplate();
-	Event event = null;
 
 	@Override
 	public void onMessage(Message message) {
-
-		try {
-			event = mapper.readValue(message.getBody(), Event.class);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
 		
-		saveEvent(event);
+		if (message.getMessageProperties().getHeader("operation").equals("event")) {
+			
+			Event event = null;
+			
+			try {
+				event = mapper.readValue(message.getBody(), Event.class);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			
+			saveEvent(event);
+			
+		} else if (message.getMessageProperties().getHeader("operation").equals("guest")) {
+			
+			Guest guest = null;
+			long eventCode = message.getMessageProperties().getHeader("eventCode");
+			
+			try {
+				guest = mapper.readValue(message.getBody(), Guest.class);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			
+			saveGuest(eventCode, guest);
+			
+		} else { }
+		
 	}
 
 	@Override
@@ -53,7 +76,7 @@ public class BrokerListenerServiceImpl implements BrokerListenerService {
 		String path = endpointURI + "/saveEvent";
 		ResponseEntity<Event> responseEntity = restTemplate.exchange(path, HttpMethod.POST, request, Event.class);
 		
-		if (responseEntity.getStatusCode().equals(HttpStatus.CREATED)) {
+		if (responseEntity.getStatusCode().equals(HttpStatus.OK)) {
 			log.info("BrokerListenerServiceImpl:saveEvent() - Event sent to EventoWS API with successfull!");
 		} else {
 			log.error("Error when sent Event to EventWS API!");
@@ -61,8 +84,28 @@ public class BrokerListenerServiceImpl implements BrokerListenerService {
 	}
 
 	@Override
-	public void saveGuest(long guestCode, Guest guest) {
-		// TODO Auto-generated method stub
+	public void saveGuest(long eventCode, Guest guest) {
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+		HttpEntity<Guest> request = new HttpEntity<>(guest, headers);
+		String path = endpointURI + "/saveGuest/{eventCode}";
+
+		Map<String, Long> variables = new HashMap<String, Long>();
+		variables.put("eventCode", eventCode);
+		
+		URI uri = UriComponentsBuilder.fromUriString(path)
+		        .buildAndExpand(variables)
+		        .toUri();
+		
+		ResponseEntity<Guest> responseEntity = restTemplate.exchange(uri, HttpMethod.POST, request, Guest.class);
+		
+		if (responseEntity.getStatusCode().equals(HttpStatus.OK)) {
+			log.info("BrokerListenerServiceImpl:saveGuest() - Guest sent to EventoWS API with successfull!");
+		} else {
+			log.error("Error when sent Guest to EventWS API!");
+		}
 		
 	}
 }
